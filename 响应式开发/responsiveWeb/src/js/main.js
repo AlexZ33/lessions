b178e4e8ce3f99ab87241b40db756895365ad71f
AlexZ33 <775136985@qq.com>
2017-06-29 13:02:34 +0800

    添加轮播广告

---
+++ 响应式开发/responsiveWeb/src/js/main.js
@@ -0,0 +1,10 @@
+//https://owlcarousel2.github.io/OwlCarousel2/docs/api-options.html
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    items:1,
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:3000
+  });
+});
+
